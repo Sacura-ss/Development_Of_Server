4c15b3fb5842d15ed00c083360c325ffedcc9a0c
Sacura-ss <sacura-ss@yandex.ru>
2022-10-03 20:28:50 +0300

    Add some src, create apache config, modified dockerfile and docker-compose.yaml

---
+++ src/admin/1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin page</title>
+</head>
+<body>
+    <h1>Taблица покупателей</h1>
+    <?php
+    $mysqli = new mysqli("db", "user", "password", "appDB");
+    $result = $mysqli->query("SELECT * FROM users");
+        foreach ($result as $row) {
+            echo "<tr><td>{$row['ID']}</td><td>{$row['name']}</td><td>{$row['surname']}</td></tr>";
+        }
+    ?>
+</body>
+</html>
