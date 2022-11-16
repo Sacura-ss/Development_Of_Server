35bad4a35c46aadbf99f43fe356e62ef2940e442
Sacura-ss <sacura-ss@yandex.ru>
2022-11-16 21:31:10 +0300

    try work with redis

--- src/profile/index.php
+++ src/profile/index.php
@@ -1,10 +1,32 @@
 <?php
-session_start();
+
+ini_set('session.save_handler', 'redis');
+echo '' . ini_get('session.save_handler');
+
+//session_start();
 
 if ($_SESSION['user']) {
     header('Location: profile.php');
 }
 
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+$redis = new Redis();
+$redis->pconnect('redis', 6379);
+
+//Get list of all keys. This creates an array of keys from the redis-cli output of "KEYS *"
+$list = $redis->keys("*");
+//Optional: Sort Keys alphabetically
+sort($list);
+foreach ($list as $key)
+{
+	//Get Value of Key from Redis
+	$value = $redis->get($key);
+	//Print Key/value Pairs
+	echo "<b>Key:</b> $key <br /><b>Value:</b> $value <br /><br />";
+}
 ?>
 
 <!doctype html>
--- src/profile/profile.php
+++ src/profile/profile.php
@@ -3,6 +3,29 @@ session_start();
 if (!$_SESSION['user']) {
     header('Location: index.php');
 }
+
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+echo session_id();
+
+$redis = new Redis();
+$redis->pconnect('redis', 6379);
+
+$redis->set(session_id(), $_SESSION['user']);
+
+//Get list of all keys. This creates an array of keys from the redis-cli output of "KEYS *"
+$list = $redis->keys("*");
+//Optional: Sort Keys alphabetically
+sort($list);
+foreach ($list as $key)
+{
+	//Get Value of Key from Redis
+	$value = $redis->get($key);
+	//Print Key/value Pairs
+	echo "<b>Key:</b> $key <br /><b>Value:</b> $value <br /><br />";
+}
 ?>
 
 <!doctype html>
